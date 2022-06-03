74e52218a0ef87316805b89839dfe6005263e51c
shagat <shagat.bing@gmail.com>
2022-06-03 07:19:45 +0530

    using auth guard/canActivate to guard routes and its child routes. A fake auth service implementation to mimic authentication for the guard.

--- src/app/auth-guard.services.ts
+++ src/app/auth-guard.services.ts
@@ -1,24 +1,28 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
-constructor(private authService: AuthService, private router: Router) { }
+export class AuthGuard implements CanActivate, CanActivateChild{
+    constructor(private authService: AuthService) { }
 
-canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean> | Promise<boolean> {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean> | Promise<boolean> {
         return this.authService.isAuthenticated()
-        .then(
-            (authenticated: boolean) => {
-                if (authenticated) {
-                    return true;
+            .then(
+                (authenticated: boolean) => {
+                    if (authenticated) {
+                        return true;
+                    }
+                    else {
+                        // this.router.navigate(['/']);
+                        return false;
+                    }
                 }
-                else {
-                    // this.router.navigate(['/']);
-                    return false;
-                }
-            }
             );
-        }
     }
+
+    canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        return this.canActivate(childRoute, state)
+    }
+}
